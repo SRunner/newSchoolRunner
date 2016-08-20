a30f266e38afb60ac09d82953a4ee4b36c34ad1f
845968074 <845968074@qq.com>
2016-08-20 16:02:58 +0800

    student-api-validate

--- app/services/xiyou/info.js
+++ app/services/xiyou/info.js
@@ -1,37 +1,33 @@
 import request from 'superagent';
 import {User} from '../../mongodb/schema';
-function info ({userId,email,tel,sessionKey},callback) {
+function info({userId, email, tel, sessionKey}, callback) {
   request.get('http://api.xiyoumobile.com/xiyoulibv2/user/info')
     .send({session: sessionKey})
     .end((err, response) => {
-      console.log("--------info-------------");
       if (err)  console.log(err);
-      let statecode=201;
+      let statecode = 201;
       let detail = response.body.Detail;
       let name = detail.Name;
       console.log(name);
       if (name == undefined) {
         console.log("用户未登录");
-        statecode=403;
-        callback(null,statecode);
+        statecode = 403;
+        callback(null, statecode);
       }
       else {
-      let studentInformation = new User({
-        name: name,
-        userId: userId,
-        email: email,
-        tel: tel
-      });
-      console.log("-------save-----------")
-      studentInformation.save(function (err, student) {
-        console.log("-----student----------------");
-        if (err) return next(err);
-        console.log("Student: " + student);
-        statecode=201;
-        console.log("savecoed:" +statecode);
-        return statecode;
-      });}
-      callback(null,statecode);
+        let studentInformation = new User({
+          name: name,
+          userId: userId,
+          email: email,
+          tel: tel
+        });
+        studentInformation.save(function (err, student) {
+          if (err) return next(err);
+          statecode = 201;
+          return statecode;
+        });
+      }
+      callback(null, statecode);
     })
 }
 export default info;
